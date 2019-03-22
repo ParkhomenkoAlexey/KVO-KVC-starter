03c48f111d2231f81b9b49947302481e9a15f1c1
Алексей Пархоменко <parkhomenko_ad@mail.ru>
2019-03-22 13:11:17 +0300

    Initial Commit

---
+++ KVO&KVC SwiftBook.ru/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  KVO&KVC SwiftBook.ru
+//
+//  Created by Алексей Пархоменко on 22/03/2019.
+//  Copyright © 2019 Алексей Пархоменко. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var textLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    @IBAction func buttonTapped(_ sender: Any) {
+    }
+    
+    @IBAction func textFieldEditingChanged(_ sender: Any) {
+    }
+    
+}
+
